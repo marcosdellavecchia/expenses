bbc47abbb728f95134a176a858c0ca708c859b22
marcosdellavecchia <marcosdvecchia@gmail.com>
2022-04-23 10:58:54 -0300

    add splashscreen for android

--- android/app/src/main/java/com/expenses/MainActivity.java
+++ android/app/src/main/java/com/expenses/MainActivity.java
@@ -1,8 +1,17 @@
 package com.expenses;
 
 import com.reactnativenavigation.NavigationActivity;
+import android.graphics.drawable.Drawable;
+import androidx.core.content.ContextCompat;
+import android.widget.LinearLayout;
 
 public class MainActivity extends NavigationActivity {
 
-  
+    @Override
+    protected void addDefaultSplashLayout() {
+        LinearLayout splash = new LinearLayout(this);
+        Drawable launch_screen_bitmap = ContextCompat.getDrawable(getApplicationContext(), R.drawable.launch_screen_bitmap);
+        splash.setBackground(launch_screen_bitmap);
+        setContentView(splash);
+    }
 }
